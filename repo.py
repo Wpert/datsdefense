b8ee3a1560b6cc3227e46c1425c06e9fff459bd7
Andrey Kalikin <kinzo.x.beato@gmail.com>
2024-07-12 22:04:27 +0300

    now tracking gold and time to move

--- repo.py
+++ repo.py
@@ -9,18 +9,23 @@ class Repo:
         self.baseCells = []
         self.enemyCells = []
         self.zombies = []
+        self.gold = 0
 
         self.base = "https://games-test.datsteam.dev"
         self.headers = {"X-Auth-Token": os.environ.get("API_KEY", "invalid_key")}
 
     def begin(self):
-        requests.put(self.base + "/play/zombidef/participate", headers=self.headers)
+        p = requests.put(self.base + "/play/zombidef/participate", headers=self.headers)
+
         r = requests.get(self.base + "/play/zombidef/world", headers=self.headers)
         self.zpots = r.json()['zpots']
         self.update()
+        return p.json()['startsInSec']
 
     def update(self):
         r = requests.get(self.base + "/play/zombidef/units", headers=self.headers)
         self.baseCells = r.json()['base']
         self.enemyCells = r.json()['enemyBlocks']
         self.zombies = r.json()['zombies']
+        self.gold = r.json()['player']['gold']
+        return r.json()['turnEndsInMs']
